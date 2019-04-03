42cba60e287d3e9aee2a4c7a8c3fc4cb128a3eed
ishi720 <ishizuka720@gmail.com>
2019-04-03 16:05:38 +0900

    Update run_test.sh

--- kc-php7/run_test.sh
+++ kc-php7/run_test.sh
@@ -65,7 +65,7 @@ done
 e "php -i | grep -i docker-php-ext-zip.ini"
 e "php -i | grep -i Libzip"
 e "php -i | grep -i 'API Extensions => mysqli'"
-#e "php -i | grep -i 'mysqli.allow_local_infile' | grep -i On"
+e "php -i | grep -i 'mysqli.allow_local_infile' | grep -i On"
 
 
 echo
